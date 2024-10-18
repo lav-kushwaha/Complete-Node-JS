42d3cb2781d5816fa0ab1432c02d98080edfb5af
Lav Kushwaha <lavkumar1224@gmail.com>
2024-10-18 17:22:38 +0530

    Lets write code

---
+++ 03_Lets_write_code/script.js
@@ -0,0 +1,13 @@
+// console.log("Hello world");
+
+// // In Node.js, `global` is the global object
+// console.log(global); 
+
+// // `this` refers to an empty object in the module scope of Node.js (not the global object)
+// console.log(this); 
+
+// // `globalThis` refers to the global object in any environment (both Node.js and browsers)
+// console.log(globalThis);
+
+//Both `global` and `globalThis` point to the global object in Node.js
+// console.log(global === globalThis); // true
