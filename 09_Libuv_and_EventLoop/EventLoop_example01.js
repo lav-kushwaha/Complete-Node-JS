9752138c49aa12888f94c6a03c0e2f50f7606b1b
Lav Kushwaha <lavkumar1224@gmail.com>
2024-11-18 15:17:51 +0530

    Libuv and event loop

---
+++ 09_Libuv_and_EventLoop/EventLoop_example01.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const a = 100;
+
+setImmediate(() => console.log("setImmediate"));
+
+//This readFile line of code execute in next cycle of event loop.
+//This file reading will take some time to read file and execute.
+fs.readFile("./file.txt", "utf8", () => {
+  console.log("File Reading CB");
+});
+
+setTimeout(() => console.log("Timer expired"), 0);
+
+function printA() {
+  console.log("a=", a);
+}
+
+printA();
+console.log("Last line of the file.");
+
+// a=100
+// Last line of the file.
+// Timer expired
+// setImmediate
+// File Reading CB
