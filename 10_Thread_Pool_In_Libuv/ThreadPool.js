5e8916d67dd60de43eda54e2dae4fc17e6334fd9
Lav Kushwaha <lavkumar1224@gmail.com>
2024-11-26 16:39:29 +0530

    Thread Pool in Node js

--- 10_Thread_Pool_In_Libuv/ThreadPool.js
+++ 10_Thread_Pool_In_Libuv/ThreadPool.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const crypto = require("crypto");
 
 //changing thread pool size
-process.env.UV_THREADPOOL_SIZE = 2;
+// process.env.UV_THREADPOOL_SIZE = 2;
 
 //every crypto is assign one thread pool and there are total 4 thread, but when we use more than 4 crypto.
 //then it will wait for one thread to be execute and empty, then other crypto assign to empty thread and execute.
