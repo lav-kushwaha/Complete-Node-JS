af53a0cb16eff95d79758e4562e5ba52d8f6c240
Lav Kushwaha <lavkumar1224@gmail.com>
2024-11-28 16:17:17 +0530

    creating url in server

--- 11_Creating_a_server/server.js
+++ 11_Creating_a_server/server.js
@@ -1,7 +1,10 @@
 const http = require('node:http');
 
 const server = http.createServer((req,res)=>{
-        res.end("hello world")
+    if(req.url==="/getsecretdata"){
+        res.end("there is no secret data");
+    }
+    res.end("hello world")
 });
 
 server.listen(3000);
