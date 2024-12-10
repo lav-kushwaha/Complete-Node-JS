dd2b7bda734790271e4b936612e541b45f85a03c
Lav Kushwaha <lavkumar1224@gmail.com>
2024-12-10 15:45:51 +0530

    Multiple routes handlers

--- 17_Middlewares_And_ErrorHandlers/src/MultipleRoutesHandlers.js
+++ 17_Middlewares_And_ErrorHandlers/src/MultipleRoutesHandlers.js
@@ -35,11 +35,32 @@ app.use('/user', (req, res, next) => {
 }, (req, res) => {
     // route handler2
     console.log("Handling the route user2");
+
+    //Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
     res.send("Response 2"); // This will NOT be reached because the response has already been sent in handler1
 });
 
 
-// Case 3: In this, when we request the /user route, it will call the first route handler
+// Case 3: In this, when we request the /user route, the first route handler will log to the console and
+// explicitly call `next()`, which passes control to the second route handler. 
+// The second route handler executes first and successfully sends a response with `res.send("Response 2")`.
+// However, since `res.send("Response!!")` is called after `next()`, the first handler attempts to send the response
+// after the second handler has already completed the response cycle, resulting in an error.
+
+app.use('/user', (req, res, next) => {
+    // route handler1
+    console.log("Handling the route user");
+    next(); // This passes control to the second route handler
+
+    //Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+    res.send("Response!!"); // This sends the response, but will cause an error because the response cycle is already completed and sent once
+}, (req, res) => {
+    // route handler2
+    console.log("Handling the route user2");
+    res.send("Response 2"); // This will be sent first, and then handler1 will fail when trying to send another response
+});
+
+// Case 4: In this, when we request the /user route, it will call the first route handler
 // which will execute and log to the console, and we are not sending any response from the first route handler.
 // The question is: will it go to the next route handler? No, it won't go to the next route handler
 // unless we explicitly call `next()` to pass control to the next handler.
@@ -53,7 +74,7 @@ app.use('/user', (req, res) => {
     res.send("Response 2"); // This won't be reached because we have not used next() to pass control to the next handler
 });
 
-// Case 4: In this, when we request the /user route, it will call the first route handler
+// Case 5: In this, when we request the /user route, it will call the first route handler
 // which will execute and log to the console, and we are not sending any response from the first route handler.
 // The question is: will it go to the next route handler? Yes, it will go to the next route handler
 // because we explicitly call next() to pass control.
@@ -73,8 +94,6 @@ app.use('/user', (req, res, next) => {
 // 1. Once `res.send()` is called, the response is sent, and no more route handlers are executed unless you call `next()` explicitly.
 // 2. You should only send one response per request to avoid errors. Calling `res.send()` multiple times will result in an error.
 // 3. If you don't call `next()` in a route handler, the request will not proceed to the next handler, and the response will not be sent until `next()` is called or you send a response in the current handler.
-
-
 
 //This listen method accept the req from client side and then response to the client.
 app.listen(3000,()=>{
