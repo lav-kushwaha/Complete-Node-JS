a7ba1b7cf91d90ae6e9642e20f36d86472e22870
Lav Kushwaha <lavkumar1224@gmail.com>
2024-12-14 22:18:00 +0530

    middleware

--- 17_Middlewares_And_ErrorHandlers/src/app.js
+++ 17_Middlewares_And_ErrorHandlers/src/app.js
@@ -9,17 +9,39 @@ const app = express();
 // If we use a slash ("/") in `app.use()`, it will handle all requests to the root path and any subsequent paths.
 // The middleware will catch requests that start with "/", and since it sends a response,
 // the request-response cycle is completed here, and control will not pass to any subsequent route handler.
-app.use("/",(req,res)=>{
-    res.send("handle/route");
+// app.use("/",(req,res,next)=>{
+//     res.send("handle/route");
+        // next()
+// });
+
+// app.get(
+//     "/user",
+//     (req,res,next)=>{//middleware
+//     console.log("Route handler 1");
+//     next();
+// },(req,res,next)=>{//middleware
+//     next();
+// },
+// (req,res)=>{//request handler bcs its sending the data back!
+//     console.log("Route handler 2");
+//     res.send("Response!!");
+// });
+
+
+//Note : main job of server is to sending the data back or response to the client.
+
+
+//Example :
+app.get("/admin/getAllData",(req,res)=>{
+    const token = "xyz";
+    if(token==="xyz"){
+        res.send("get your data");
+    }else{
+        res.status(401).send("Invalid!!!");
+    }
 });
 
-app.get("/user",(req,res)=>{
-    console.log("Route handler 1");
-    res.send("Response 1");
-},(req,res)=>{
-    console.log("Route handler 2");
-    res.send("Response 2");
-});
+
 
 //This listen method accept the req from client side and then response to the client.
 app.listen(3000,()=>{
