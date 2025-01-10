2ded8744e9bf80afc4d2d225823556afd39ca3c7
Lav Kushwaha <lavkumar1224@gmail.com>
2025-01-10 11:17:45 +0530

    Authentications JWT and Cookies

---
+++ 22_Authentication_JWT_And_Cookies/src/models/user.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        minLength:3, //for string we use minLength and maxLength
+        maxLength:100,
+    },
+    lastName:{
+        type:String
+    },
+    emailId:{
+        type:String,
+        lowercase:true,
+        required:true,
+        unique:true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){ //validator
+                throw new Error("Invalid email address: " + value);
+            }
+        },
+    },
+    password:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password:"+ value);
+            }
+        }
+    },
+    age:{
+        type:Number,
+        min:18,//for num we use min
+    },
+    gender:{
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid!");
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://conferenceoeh.com/wp-content/uploads/profile-pic-dummy.png",
+        validate(value){
+            if(!validator.isURL(value)){ //validator
+                throw new Error("Invalid photo URL : " + value);
+            }
+        }
+    },
+    about:{
+        type:String,
+        default:"This is a default about of the user!"
+    },
+    skills:{
+        type:[String],
+    }
+},
+{
+    timestamps:true,
+});
+
+// const User = mongoose.model("User",userSchema);
+
+module.exports = mongoose.model("User",userSchema);
