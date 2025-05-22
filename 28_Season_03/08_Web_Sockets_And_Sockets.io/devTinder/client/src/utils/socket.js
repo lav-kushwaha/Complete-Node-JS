f48ca9527dc71019f5e60731ee0c40e208299dd1
Lav Kushwaha <lavkumar1224@gmail.com>
2025-05-23 00:07:31 +0530

    Implemented Chat Features Using Socket.Io

--- 28_Season_03/08_Web_Sockets_And_Sockets.io/devTinder/client/src/utils/socket.js
+++ 28_Season_03/08_Web_Sockets_And_Sockets.io/devTinder/client/src/utils/socket.js
@@ -2,5 +2,5 @@ import io from "socket.io-client"
 import {BASE_URL} from "./Constant"
 
 export const createSocketConnection = ()=>{
-    return io(BASE_URL);
+    return io(BASE_URL);//connecting to the backend.
 }
--- 28_Season_03/08_Web_Sockets_And_Sockets.io/devTinder/server/src/Utils/socket.js
+++ 28_Season_03/08_Web_Sockets_And_Sockets.io/devTinder/server/src/Utils/socket.js
@@ -1,33 +1,31 @@
 const socket = require("socket.io");
 
-const initializeSocket = (server) =>{
-   const io = socket(server,{
-    cors:{
-        origin:"http://localhost:5173",
+const initializeSocket = (server) => {
+  const io = socket(server, {
+    cors: {
+      origin: "http://localhost:5173",
     },
-   });
-
-   io.on("connection", (socket)=>{
-    
-    //handle events.
-    socket.on("joinChat",({userId, targetUserId})=>{
-        const roomId = [userId, targetUserId].sort().join("_");
-        console.log("Joining Room : " + roomId)
-        socket.join(roomId);
-    });
-
-    socket.on(
-    "sendMessage",
-    ({firstName, userId, targetUserId, text})=>{
-        const roomId = [userId,targetUserId].sort().join("_");
-        io.to(roomId).emit("messageReceived",{firstName, text});
-
-    });
-
-    socket.on("disconnect",()=>{});
-    
   });
 
+  io.on("connection", (socket) => {
+    // User joins a chat room
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const roomId = [userId, targetUserId].sort().join("_");
+      console.log("Joining Room:", firstName, ":", roomId);
+      socket.join(roomId);
+    });
+
+    // Handle sending message
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+      const roomId = [userId, targetUserId].sort().join("_");
+      io.to(roomId).emit("messageReceived", { firstName, text });
+    });
+
+    // Handle disconnect
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
+  });
 };
 
-module.exports = {initializeSocket}
+module.exports = { initializeSocket };
